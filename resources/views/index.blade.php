56d7c45cafe6bb14c57094487eccb097e0c27c06
pcerbino <pcerbino@gmail.com>
2022-11-04 18:47:09 -0300

    asset in api name url

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,7 @@
         {
         $.ajax({
             type: 'GET',
-                url: 'http://127.0.0.1:8000/api/random',
+                url: {{ asset('api/random') }},
             })
             .done(
                 function(api)
